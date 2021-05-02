4dc183637548c60adc92fbed6e80b8f8c7caec23
johnmburu761 <83529531+johnmburu761@users.noreply.github.com>
2021-05-02 15:48:33 +0300

    Add files via upload

--- resources.js
+++ resources.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById("home__btn");
 const cpd = document.getElementById("copyrights");
 btn.addEventListener("click",() => {
-	window.location.href = "https://www.johnmburu761.github.io";
+	window.location.href = "https://johnmburu761.github.io/";
 })
 
 cpd.appendChild(document.createTextNode(new Date().getFullYear()));
